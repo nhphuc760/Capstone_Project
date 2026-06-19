47747d59815c0ca9eb435a14d5e2a9f697454f24
nhphuc760 <dkmtaoko@gmail.com>
2026-06-19 22:21:53 +0700

    update

--- Assets/Scripts/Networking/NetworkRunnerHandler.cs
+++ Assets/Scripts/Networking/NetworkRunnerHandler.cs
@@ -1,10 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Fusion;
 using Unity.VisualScripting;
 using TriInspector;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using Fusion.Sockets;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class NetworkRunnerHandler : MonoBehaviour
 {
     [SerializeField]
@@ -24,43 +26,69 @@ public class NetworkRunnerHandler : MonoBehaviour
     }
 
 
-    public async UniTask<StartGameResult> StartGame()
+    public async UniTask<StartGameResult> StartSession( string sessionName, int playerCount, Scene sceneStart, byte[] connectionToken = default,  Dictionary<string, SessionProperty> sessionProperties = null, System.Action<NetworkRunner> onGameStarted = null, System.Action<NetworkRunner> hostmigrationResume = null, string customLobbyName = "Standard" )
     {
+        await InitialRunner();
         return await _runner.StartGame(new StartGameArgs
         {
             GameMode = GameMode.AutoHostOrClient,
             Address = NetAddress.Any(),
-            Scene = null,
-            SessionName = "",
-            SceneManager = null,
-            SessionProperties = default,
-            PlayerCount = default,
-            CustomLobbyName = default,
-            OnGameStarted = default,
-            HostMigrationToken = default,
-            HostMigrationResume = default,
-            ConnectionToken = default,
+            CustomLobbyName = customLobbyName,            
+            SessionName = sessionName,
+            SessionProperties = sessionProperties,
+            SceneManager = GetSceneManager(),
+            Scene = SceneRef.FromIndex(sceneStart.buildIndex),
+            ConnectionToken = connectionToken,
+            PlayerCount = playerCount,
+            OnGameStarted = onGameStarted,
+            HostMigrationResume = hostmigrationResume,            
         });
     }
     
     public void JoinLobby(SessionLobby sessionLobby, string lobbyID = "OurLobbyID")
-    {
-        _runner.JoinSessionLobby(sessionLobby, lobbyID);
+    {        
+        _runner.JoinSessionLobby(sessionLobby, lobbyID);       
     }
 
-    void InstantiateNetworkRunner()
+    public void JoinSession()
     {
-        if (_runner != null)
+
+    }
+    public void CreateSession()
+    {
+
+    }
+
+    async UniTask InitialRunner()
+    {
+        if (_runner != null && _runner.IsRunning)
         {
-            Destroy(_runner.gameObject);
+            await _runner.Shutdown();         
             _runner = null;
         }
-        _runner = Instantiate(NetworkRunnerPrefab);
-        var scene = _runner.GetComponent<INetworkSceneManager>();
-        if (scene == null)
+        if (_runner == null)
         {
-            scene = _runner.AddComponent<NetworkSceneManagerDefault>();
+            _runner = Instantiate(NetworkRunnerPrefab);
+            InitialRunnerCallbacks();
         }
+    } 
+    void InitialRunnerCallbacks()
+    {
+        if (_runner == null) return;
+        if (!_runner.TryGetComponent<INetworkRunnerCallbacks>(out var callbacks))
+        {
+            _runner.AddComponent<BasicSpawner>();
+        }
+    }   
+
+    INetworkSceneManager GetSceneManager()
+    {
+        var sceneManager = _runner.GetComponent<INetworkSceneManager>();
+        if (sceneManager == null)
+        {
+            sceneManager = _runner.AddComponent<NetworkSceneManagerDefault>();
+        }
+        return sceneManager;
     }
 
 
