5b82f636331c3add728cda0a8bd0642db810ffa6
GiaHuy1106 <giahuy110600@gmail.com>
2026-06-25 12:48:46 +0700

    + updating email login

---
+++ Assets/Scripts/FirebaseScript/EmailLogin.cs
@@ -0,0 +1,260 @@
+using UnityEngine;
+using TMPro;
+using Firebase.Auth;
+using Firebase;
+using Firebase.Extensions;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EmailLogin : MonoBehaviour
+{
+    [Header("Login")]
+    public TMP_InputField LoginEmail;
+    public TMP_InputField LoginPassword;
+
+    [Header("Sign up")]
+    public TMP_InputField SignUpEmail;
+    public TMP_InputField SignUpPassword;
+    public TMP_InputField SignUpEmailConfirm;
+
+    [Header("Extra")]
+    //public GameObject loadingScreen;
+    public GameObject NoficationPanel;
+    public Toggle rememberMeToggle;
+    public TextMeshProUGUI logTxt; 
+    public GameObject loginUI, signUpUI, SuccessUI;
+
+    // sign up
+    public void SignUp()
+    {
+        //loadingScreen.SetActive(true);
+
+        // Create a new user with email and password
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+        string email = SignUpEmail.text;
+        string password = SignUpPassword.text;
+
+        // Check if the email and password fields are not empty
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignUp was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignUp encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Sign up successful
+            //loadingScreen.SetActive(false);
+            AuthResult result = task.Result;
+            Debug.LogFormat("User signed up successfully: {0} ({1})", 
+                result.User.DisplayName, result.User.UserId);
+
+            SignUpEmail.text = "";
+            SignUpPassword.text = "";
+            SignUpEmailConfirm.text = "";
+
+            if (result.User.IsEmailVerified)
+            {
+                showLogMsg("Sign up successful! Please log in.");
+                
+            }
+            else
+            {
+
+                openNotificationPanel();
+                SendEmailVerification();        
+            }
+        });
+
+        signUpUI.SetActive(false);
+        loginUI.SetActive(true);
+        
+    }
+
+    //Email Confirmation 
+    public void SendEmailVerification()
+    {
+        StartCoroutine(SendEmailVerificationAsync());
+    }
+
+    IEnumerator SendEmailVerificationAsync()
+    {
+        FirebaseUser user = FirebaseAuth.DefaultInstance.CurrentUser;
+        if (user != null)
+        {
+            var sendEmailTask = user.SendEmailVerificationAsync();
+            yield return new WaitUntil(() => sendEmailTask.IsCompleted);
+
+            if (sendEmailTask.Exception != null)
+            {
+                print("Email send error");
+                FirebaseException firebaseException = sendEmailTask.Exception.GetBaseException() as FirebaseException;
+                AuthError error = (AuthError) firebaseException.ErrorCode;
+
+                switch (error)
+                {
+                    case AuthError.None: break;
+                    case AuthError.Unimplemented: break;
+                    case AuthError.Failure: break;
+                    case AuthError.InvalidCustomToken: break;
+                    case AuthError.CustomTokenMismatch: break;
+                    case AuthError.InvalidCredential: break;
+                    case AuthError.UserDisabled: break;
+                    case AuthError.AccountExistsWithDifferentCredentials: break;
+                    case AuthError.OperationNotAllowed: break;
+                    case AuthError.EmailAlreadyInUse: break;
+                    case AuthError.RequiresRecentLogin: break;
+                    case AuthError.CredentialAlreadyInUse: break;
+                    case AuthError.InvalidEmail: break;
+                    case AuthError.WrongPassword: break;
+                    case AuthError.TooManyRequests: break;
+                    case AuthError.UserNotFound: break;
+                    case AuthError.ProviderAlreadyLinked: break;
+                    case AuthError.NoSuchProvider: break;
+                    case AuthError.InvalidUserToken: break;
+                    case AuthError.UserTokenExpired: break;
+                    case AuthError.NetworkRequestFailed: break;
+                    case AuthError.InvalidApiKey: break;
+                    case AuthError.AppNotAuthorized: break;
+                    case AuthError.UserMismatch: break;
+                    case AuthError.WeakPassword: break;
+                    case AuthError.NoSignedInUser: break;
+                    case AuthError.ApiNotAvailable: break;
+                    case AuthError.ExpiredActionCode: break;
+                    case AuthError.InvalidActionCode: break;
+                    case AuthError.InvalidMessagePayload: break;
+                    case AuthError.InvalidPhoneNumber: break;
+                    case AuthError.MissingPhoneNumber: break;
+                    case AuthError.InvalidRecipientEmail: break;
+                    case AuthError.InvalidSender: break;
+                    case AuthError.InvalidVerificationCode: break;
+                    case AuthError.InvalidVerificationId: break;
+                    case AuthError.MissingVerificationCode: break;
+                    case AuthError.MissingVerificationId: break;
+                    case AuthError.MissingEmail: break;
+                    case AuthError.MissingPassword: break;
+                    case AuthError.QuotaExceeded: break;
+                    case AuthError.RetryPhoneAuth: break;
+                    case AuthError.SessionExpired: break;
+                    case AuthError.AppNotVerified: break;
+                    case AuthError.AppVerificationFailed: break;
+                    case AuthError.CaptchaCheckFailed: break;
+                    case AuthError.InvalidAppCredential: break;
+                    case AuthError.MissingAppCredential: break;
+                    case AuthError.InvalidClientId: break;
+                    case AuthError.InvalidContinueUri: break;
+                    case AuthError.MissingContinueUri: break;
+                    case AuthError.KeychainError: break;
+                    case AuthError.MissingAppToken: break;
+                    case AuthError.MissingIosBundleId: break;
+                    case AuthError.NotificationNotForwarded: break;
+                    case AuthError.UnauthorizedDomain: break;
+                    case AuthError.WebContextAlreadyPresented: break;
+                    case AuthError.WebContextCancelled: break;
+                    case AuthError.DynamicLinkNotActivated: break;
+                    case AuthError.Cancelled: break;
+                    case AuthError.InvalidProviderId: break;
+                    case AuthError.WebInternalError: break;
+                    case AuthError.WebStorateUnsupported: break;
+                    case AuthError.TenantIdMismatch: break;
+                    case AuthError.UnsupportedTenantOperation: break;
+                    case AuthError.InvalidLinkDomain: break;
+                    case AuthError.RejectedCredential: break;
+                    case AuthError.PhoneNumberNotFound: break;
+                    case AuthError.InvalidTenantId: break;
+                    case AuthError.MissingClientIdentifier: break;
+                    case AuthError.MissingMultiFactorSession: break;
+                    case AuthError.MissingMultiFactorInfo: break;
+                    case AuthError.InvalidMultiFactorSession: break;
+                    case AuthError.MultiFactorInfoNotFound: break;
+                    case AuthError.AdminRestrictedOperation: break;
+                    case AuthError.UnverifiedEmail: break;
+                    case AuthError.SecondFactorAlreadyEnrolled: break;
+                    case AuthError.MaximumSecondFactorCountExceeded: break;
+                    case AuthError.UnsupportedFirstFactor: break;
+                    case AuthError.EmailChangeNeedsVerification: break;
+                    default: break;
+                }
+            }
+            else
+            {
+                print ("Email sent successfully");
+            }
+        }
+    }
+
+    // Log in
+    public void LogIn()
+    {
+        //loadingScreen.SetActive(true);
+
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+        string email = LoginEmail.text;
+        string password = LoginPassword.text;
+
+        Credential credential = EmailAuthProvider.GetCredential(email, password);
+        auth.SignInAndRetrieveDataWithCredentialAsync(credential).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("LogIn was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("LogIn encountered an error: " + task.Exception);
+                return;
+            }
+
+            //loadingScreen.SetActive(false);
+            AuthResult result = task.Result;
+            Debug.LogFormat("User logged in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+
+            if (result.User.IsEmailVerified)
+            {
+                showLogMsg("Login successful!");
+                loginUI.SetActive(false);
+                SuccessUI.SetActive(true);
+                SuccessUI.transform.Find("SuccessText").GetComponent<TextMeshProUGUI>().text = "Welcome, " + result.User.Email + "!";
+            }
+            else
+            {
+                showLogMsg("please verify email");
+            }
+        });
+    }
+
+     // Display a message to the user in the UI log text
+    private void showLogMsg(string message)
+    {
+        if (logTxt != null)
+        {
+            logTxt.text = message;
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+    }
+
+    public void closeNotificationPanel()
+    {
+        NoficationPanel.SetActive(false);
+    }
+
+    private void openNotificationPanel()
+    {
+        NoficationPanel.SetActive(true);
+        showLogMsg("Please verify your email before logging in. A verification email has been sent to your email address.");
+    }
+
+    public void ToggleRememberMe()
+    {
+        
+    }
+}
