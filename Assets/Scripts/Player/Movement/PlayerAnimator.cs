1cea0e722f887a686ca65e536ce880f40f894987
DungLuong1602 <ITCSIU24024@student.hcmiu.edu.vn>
2026-08-24 14:17:15 +0700

    add pick up movement

--- Assets/Scripts/Player/Movement/PlayerAnimator.cs
+++ Assets/Scripts/Player/Movement/PlayerAnimator.cs
@@ -10,6 +10,9 @@ public class PlayerAnimator : MonoBehaviour
     private readonly int isGrabbingHash = Animator.StringToHash("IsGrabbing");
     private readonly int punchHash = Animator.StringToHash("Punch");
 
+    private readonly int GetItemHash = Animator.StringToHash("PickUp");
+    private readonly int DropItemHash = Animator.StringToHash("Drop");
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -28,5 +31,19 @@ public class PlayerAnimator : MonoBehaviour
     public void TriggerPunch()
     {
         animator.SetTrigger(punchHash);
+    }
+
+    [Rpc(RpcSources.InputAuthority| RpcSources.StateAuthority, RpcTargets.All)]
+    public void RPC_PlayPickUpAnimation()
+    {
+        if (animator == null) return;
+        animator.SetTrigger(GetItemHash);
+    }
+
+    [Rpc(RpcSources.InputAuthority| RpcSources.StateAuthority, RpcTargets.All)]
+    public void RPC_PlayDropAnimation()
+    {
+        if (animator == null) return;
+        animator.SetTrigger(DropItemHash);
     }
 }
